cc74c83d319b3a7228b7bb6106afc777e19e532a
Eric Tschetter <cheddar@tidepool.org>
2014-04-04 16:33:41 -0500

    Increase new space a bit

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -eu
 
 . config/env.sh
-exec node --trace_gc --max_new_space_size=16384 --max_old_space_size=128 server.js
+exec node --trace_gc --max_new_space_size=32768 --max_old_space_size=128 server.js
